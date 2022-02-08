9999b51de90da7683dcaa4e491005c9e9986538a
berkanturkali <brktrkali@outlook.com>
2022-02-08 07:54:39 +0300

    migrate app build.gradle to kotlin dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    androidApplication
+    kotlinAndroid
+}
+
+android {
+    compileSdk = Config.Version.compileSdkVersion
+
+    defaultConfig {
+        applicationId = Config.Android.applicationId
+        minSdk = Config.Version.minSdkVersion
+        targetSdk = Config.Version.targetSdkVersion
+        versionCode = Config.Version.versionCode
+        versionName = Config.Version.versionName
+        multiDexEnabled = Config.isMultiDexEnabled
+        testInstrumentationRunner = Config.Android.testInstrumentationRunner
+        vectorDrawables {
+            useSupportLibrary = Config.useSupportLibrary
+        }
+    }
+
+    buildTypes {
+        named(BuildType.DEBUG) {
+            isMinifyEnabled = false
+            proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
+        }
+        named(BuildType.RELEASE) {
+            isMinifyEnabled = true
+            proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = composeVersion
+    }
+    packagingOptions {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+}
+
+dependencies {
+    implementAll(Dependencies.AndroidX.components)
+    implementAll(Dependencies.Compose.components)
+    implementation(Dependencies.Compose.activity)
+    testImplementation(Dependencies.Test.junit)
+    androidTestImplementation(Dependencies.Test.junitExt)
+    androidTestImplementation(Dependencies.Test.espresso)
+    androidTestImplementation(Dependencies.Test.composeJunit)
+    debugImplementation(Dependencies.Test.composeUiTooling)
+}
