16054fe09813621cd50905a8cbd0f9971c69c573
berkanturkali <brktrkali@outlook.com>
2022-02-08 13:25:18 +0300

    add RemoteModelMapper

---
+++ remote/src/main/java/com/example/jetgames/remote/mapper/RemoteModelMapper.kt
@@ -0,0 +1,9 @@
+package com.example.jetgames.remote.mapper
+
+public interface RemoteModelMapper<in M, out D> {
+    public fun mapFromModel(model: M?): D
+
+    public fun mapModelList(models: List<M>?): List<D?>? {
+        return models?.mapTo(mutableListOf(), ::mapFromModel)
+    }
+}
