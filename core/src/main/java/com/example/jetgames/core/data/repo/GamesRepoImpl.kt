9ab6df9249579d28060fc6a8ed62d20e17f1dff5
berkanturkali <brktrkali@outlook.com>
2022-02-10 13:54:49 +0300

    add GamesRepoImpl

---
+++ core/src/main/java/com/example/jetgames/core/data/repo/GamesRepoImpl.kt
@@ -0,0 +1,30 @@
+package com.example.jetgames.core.data.repo
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.example.jetgames.core.data.contract.GamesRemote
+import com.example.jetgames.core.domain.model.games.Game
+import com.example.jetgames.core.domain.repo.GamesRepo
+import com.example.jetgames.core.remote.mapper.games.GameMapper
+import com.example.jetgames.core.remote.paging.GamesPagingSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GamesRepoImpl @Inject constructor(
+    private val gamesRemote: GamesRemote,
+    private val gameMapper: GameMapper,
+) : GamesRepo {
+    override fun fetchGames(): Flow<PagingData<Game>> {
+        return Pager(
+            config = PagingConfig(pageSize = 20, enablePlaceholders = false),
+            pagingSourceFactory = {
+                GamesPagingSource(gamesRemote = gamesRemote)
+            }
+        ).flow.map {
+            it.map(gameMapper::mapFromModel)
+        }
+    }
+}
