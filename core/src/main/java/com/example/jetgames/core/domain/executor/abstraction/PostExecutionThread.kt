ce6b576a6736547cae05b4ef332b82faa8fc6c3d
berkanturkali <brktrkali@outlook.com>
2022-02-11 14:11:23 +0300

    add PostExecutionThread

---
+++ core/src/main/java/com/example/jetgames/core/domain/executor/abstraction/PostExecutionThread.kt
@@ -0,0 +1,9 @@
+package com.example.jetgames.core.domain.executor.abstraction
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+interface PostExecutionThread {
+    val main: CoroutineDispatcher
+    val io: CoroutineDispatcher
+    val default: CoroutineDispatcher
+}
