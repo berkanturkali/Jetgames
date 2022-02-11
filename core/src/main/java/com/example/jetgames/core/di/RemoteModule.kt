311cb38c4a11e244a7767d1ce9cd34cfa7f00dee
berkanturkali <brktrkali@outlook.com>
2022-02-11 14:12:54 +0300

    add RemoteModule

---
+++ core/src/main/java/com/example/jetgames/core/di/RemoteModule.kt
@@ -0,0 +1,27 @@
+package com.example.jetgames.core.di
+
+import com.example.jetgames.core.BuildConfig
+import com.example.jetgames.core.data.contract.GamesRemote
+import com.example.jetgames.core.remote.impl.GamesRemoteImpl
+import com.example.jetgames.core.remote.service.ApiService
+import com.example.jetgames.remote.factory.RemoteFactory
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@[Module InstallIn(SingletonComponent::class)]
+interface RemoteModule {
+
+    @get:Binds
+    val GamesRemoteImpl.bindGamesRemote: GamesRemote
+
+    companion object {
+        @[Provides Singleton]
+        fun apiService(remoteFactory: RemoteFactory): ApiService =
+            remoteFactory.createRetrofit(BuildConfig.BASE_URL, BuildConfig.DEBUG)
+                .create(ApiService::class.java)
+    }
+}
