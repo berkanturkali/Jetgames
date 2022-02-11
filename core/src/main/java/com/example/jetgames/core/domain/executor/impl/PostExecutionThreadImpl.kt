4ec9263c7f64abffbbb7efcbddec75425f25a7b3
berkanturkali <brktrkali@outlook.com>
2022-02-11 14:11:52 +0300

    add PostExecutionThreadImpl

---
+++ core/src/main/java/com/example/jetgames/core/domain/executor/impl/PostExecutionThreadImpl.kt
@@ -0,0 +1,15 @@
+package com.example.jetgames.core.domain.executor.impl
+
+import com.example.jetgames.core.domain.executor.abstraction.PostExecutionThread
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+class PostExecutionThreadImpl @Inject constructor(): PostExecutionThread {
+    override val main: CoroutineDispatcher
+        get() = Dispatchers.Main
+    override val io: CoroutineDispatcher
+        get() = Dispatchers.IO
+    override val default: CoroutineDispatcher
+        get() = Dispatchers.Default
+}
