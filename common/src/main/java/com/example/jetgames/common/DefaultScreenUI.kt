de04a803c7b6453156e8f7325c7dcce914c7fa20
berkanturkali <brktrkali@outlook.com>
2022-02-11 15:35:48 +0300

    add DefaultScreenUI

---
+++ common/src/main/java/com/example/jetgames/common/DefaultScreenUI.kt
@@ -0,0 +1,25 @@
+package com.example.jetgames.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DefaultScreenUI(
+    toolbar: (@Composable () -> Unit)? = null,
+    content: @Composable () -> Unit,
+) {
+    val scaffoldState = rememberScaffoldState()
+    Scaffold(scaffoldState = scaffoldState, topBar = { toolbar?.invoke() }) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)) {
+            content()
+        }
+    }
+}
---
+++ home/src/main/java/com/example/jetgames/home/ui/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.example.jetgames.home.ui
+
+import androidx.compose.runtime.Composable
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.jetgames.core.domain.model.games.Game
+import com.example.jetgames.home.viewmodel.HomeViewModel
+
+@Composable
+fun Home(
+    viewModel:HomeViewModel
+){
+
+    val games:LazyPagingItems<Game> = viewModel.games.collectAsLazyPagingItems()
+}
