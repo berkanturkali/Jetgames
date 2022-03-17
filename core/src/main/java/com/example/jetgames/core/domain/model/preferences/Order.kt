1151eea140e66e5fb8d79694fd811b58c5facc09
berkanturkali <brktrkali@outlook.com>
2022-03-17 06:33:25 +0300

    add Order enum class

---
+++ core/src/main/java/com/example/jetgames/core/domain/model/preferences/Order.kt
@@ -0,0 +1,8 @@
+package com.example.jetgames.core.domain.model.preferences
+
+enum class Order(val value:String) {
+    NAME("name"),
+    RELEASED("released"),
+    RATING("rating"),
+    METACRITIC("metacritic")
+}
