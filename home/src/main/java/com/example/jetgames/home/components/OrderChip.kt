47c8e5ba5128f5024d36b7e294c5f64f7b5a58f1
berkanturkali <brktrkali@outlook.com>
2022-03-17 07:39:02 +0300

    add OrderChip

---
+++ home/src/main/java/com/example/jetgames/home/components/OrderChip.kt
@@ -0,0 +1,48 @@
+package com.example.jetgames.home.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ChipDefaults
+import androidx.compose.material.FilterChip
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetgames.common.ui.theme.JetgamesTheme
+import com.example.jetgames.common.ui.theme.LightGray
+
+@Composable
+fun OrderChip(
+    modifier: Modifier = Modifier,
+    name: String,
+    isSelected: Boolean,
+    onSelectionChange: (String) -> Unit = {},
+) {
+    FilterChip(
+        modifier = modifier,
+        shape = CircleShape,
+        border = if (!isSelected) BorderStroke(0.5.dp, color = LightGray) else null,
+        colors = ChipDefaults.filterChipColors(
+            backgroundColor = Color.Transparent,
+            selectedBackgroundColor = MaterialTheme.colors.secondary,
+            selectedContentColor = MaterialTheme.colors.onSecondary,
+        ),
+        selected = isSelected,
+        selectedIcon = { SelectedOrderChipIcon() },
+        onClick = {
+            onSelectionChange(name)
+        }) {
+        Text(text = name)
+    }
+}
+
+@Preview
+@Composable
+fun OrderChipPrev() {
+    JetgamesTheme {
+        OrderChip(name = "Chip", isSelected = true)
+    }
+}
